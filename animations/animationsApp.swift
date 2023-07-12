f62a69aa48776d9024a58db453521550991160dc
Till-Hemmerich-Syntax <till.hemmerich@syntax-institut.de>
2023-07-12 11:06:05 +0200

    Initial Commit

---
+++ animations/animationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  animationsApp.swift
+//  animations
+//
+//  Created by Till Hemmerich on 12.07.23.
+//
+
+import SwiftUI
+
+@main
+struct animationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

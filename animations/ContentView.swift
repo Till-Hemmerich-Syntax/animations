d8a51f2dc290c55bad67f0a66a25a3ca9aedcbfc
Till-Hemmerich-Syntax <till.hemmerich@syntax-institut.de>
2023-07-27 11:58:36 +0200

    .-.

--- animations/ContentView.swift
+++ animations/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var buttonX = 0.0
+    @State var buttonY = 0.0
+    @State var animateButton = false
+    @State var turnRectangle = false
+    @State var animateME = false
+    @State var isOn = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button("Click ME"){
+                animateButton.toggle()
+            }
+            .offset(y:animateButton ? 200 : 0)
+            
+            Button("FlipMe"){
+                turnRectangle.toggle()
+            }
+            Button("LETS GO"){
+                animateME.toggle()
+            }
+            Rectangle()
+                .frame(width: 100,height: 100)
+                .foregroundColor(.red)
+                .rotationEffect(Angle(degrees: turnRectangle ? 75 : 0))
+            Circle()
+                .frame(width: animateME ? 50 : 200)
+            Image(systemName: "square.and.arrow.up.circle")
+                .resizable()
+                .frame(width: 50,height: 50)
+                .offset(x: animateME ? 150 : 0)
+            Toggle(isOn: $isOn) {
+                Text("test")
+            }
+            .rotationEffect(Angle(degrees: animateME ? 289 : 0))
         }
+        
         .padding()
+        .animation(.spring(), value: animateButton)
+        .animation(.linear, value: turnRectangle)
+        .animation(.spring(), value: animateME)
     }
 }
 
